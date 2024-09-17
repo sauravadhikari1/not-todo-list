a41ca694d0f9ea179ee919429078957a374d9670
sauravadhikari1 <sauravadhikari456@gmail.com>
2024-09-17 20:53:48 +1000

    not-to-do finished

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let taskList=[]
+const RANDOM_STRING_LENGTH = 6;
 //getting data from the form and  adding it to the tasklist
 addTask=(myform)=>{
     const formData=new FormData(myform);
     const task=formData.get("task")
     const hour = parseInt(formData.get("hour"));
+    const id = getRandomUniqueID();
     const taskObj={
-        id:"myid",
+        id,
         task,
         hour,
         type: "good",
@@ -41,6 +43,88 @@ const displayList=()=>{
     }
     }
     )
-    goodList.innerHTML=goodListContent
+    goodList.innerHTML=goodListContent;
+    const totalHours=getTotalHours();
+    const totalHourelement=document.getElementById("total-hours")
+    totalHourelement.innerText=`The total hours allocated = ${totalHours} hours`
+
+    
+    const badList=document.getElementById("badlist")
+    let badListContent=""
+    let badIndex=0
+    taskList.map((item, index)=>{
+        if(item.type!="good"){
+        badIndex+=1;
+        badListContent+= `
+        <tr>
+        <th scope="row">${badIndex}</th>
+        <td>${item.task}</td>
+        <td>${item.hour}hrs</td>
+        <td class="text-end">
+        <button type="button" class="btn btn-warning" onclick="switchTask('${item.id}')"><i
+                class="fa-solid fa-arrow-left"></i></button>
+            <button type="button" class="btn btn-danger" onclick="deleteTask('${item.id}')"><i
+                    class="fa-solid fa-trash"></i></button>
+
+        </td>
+    </tr>
+    
+    `;
+
+    }
+    }
+    )
+badList.innerHTML=badListContent;
+let badHours=0;
+badHours=getbadHours();
+const badHourelement=document.getElementById("bad-hours")
+badHourelement.innerText=`You could have saved = ${badHours} hours`
+
+}
+function getTotalHours()
+{
+let totalHours=taskList.reduce((acc,item)=>{
+return acc+item.hour;
+},0)
+return totalHours;
+}
+
+function getbadHours()
+{
+let badHours=taskList.reduce((acc,item)=>{
+if(item.type!="good"){
+    return acc+item.hour;
+}
+else
+{
+    return 0;
+}
+},0)
+return badHours;
+}
+
+const getRandomUniqueID = () => {
+    let stringGenerator =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let randomString = "";
+
+    for (let i = 0; i < RANDOM_STRING_LENGTH; i++) {
+        let randomIndex = Math.floor(Math.random() * stringGenerator.length);
+        randomString = randomString + stringGenerator[randomIndex];
+    }
+
+    return randomString;
+};
+const switchTask = (id) => {
+    let task = taskList.find((task) => task.id == id);
+    task.type = task.type == "good" ? "bad" : "good";
+
+    displayList();
+    // displayAlert("TASK SWITCHED");
+};
+
+const deleteTask=(id)=>{
+    taskList=taskList.filter((item)=>item.id !== id)
+    displayList();
 
 }
